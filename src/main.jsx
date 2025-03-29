570db8b6ad17521fe566984fa004559871f81a39
Shivraj Kaushik <shivrajkaushik248@gmail.com>
2025-03-29 13:24:14 +0530

    debug

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Update to use 'react-dom/client'
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Use ReactDOM.createRoot for React 18+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
